912bbedaa91183928024a39a02fa0ec3e0f9e5f4
Helvetica Volubi <suisuroru@blue-millennium.fun>
2026-01-01 16:21:11 +0800

    Initial commit

---
+++ src/client/java/fun/bm/serverchunksendimporvertm/client/ServerchunksendimporvertmDataGenerator.java
@@ -0,0 +1,12 @@
+package fun.bm.serverchunksendimporvertm.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ServerchunksendimporvertmDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
---
+++ src/main/java/fun/bm/serverchunksendimporvertm/Serverchunksendimporvertm.java
@@ -0,0 +1,10 @@
+package fun.bm.serverchunksendimporvertm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Serverchunksendimporvertm implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
